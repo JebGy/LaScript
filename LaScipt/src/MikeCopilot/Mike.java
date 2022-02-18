ddb7e9e322b1facd8e099aa213d9c2a1dcbe931d
JebGy <75335038+JebGy@users.noreply.github.com>
2022-02-18 18:31:05 -0500

    Update

--- LaScipt/src/MikeCopilot/Mike.java
+++ LaScipt/src/MikeCopilot/Mike.java
@@ -8,13 +8,12 @@ import java.util.StringTokenizer;
 public class Mike {
     public static ArrayList<String> LineaCode = new ArrayList<>();
     static int i = 0;
-    public static String path="";
+    public static String path = "";
 
     public static void main(String[] args) {
-        System.out.println("Bienvenido a MikeCopilot, Soy el interprete del Lenguaje LaScript," +
-                "\nporfavor ingresa el path del archivo Lascript para ejecutar:");
-        Scanner s= new Scanner(System.in);
-        path=s.nextLine();
+        System.out.print("LaScript>> ");
+        Scanner s = new Scanner(System.in);
+        path = s.nextLine();
         Reader.setPath(path);
         try {
             Reader.leer();
@@ -31,45 +30,60 @@ public class Mike {
                 if (LineaCode.get(0).toString().equals("init;") && LineaCode.get(LineaCode.size() - 1).toString().equals("end;")) {
                     switch (LineaCode.get(i)) {
                         case "init;":
-                            System.out.println("Corriendo . . .");
+                            System.out.println("\n>>Run\n");
                             break;
                         case "sum(":
+                            System.out.println("- - - initLine - - -");
+                            int sum = 0;
+                            String data = LineaCode.get(i + 1);
 
-                            int sum=0;
-                            String data=LineaCode.get(i+1);
+                            StringTokenizer st = new StringTokenizer(data, "+");
 
-                            StringTokenizer st= new StringTokenizer(data,"+");
-
-                            while (st.hasMoreTokens()){
-                                sum+= Integer.parseInt(st.nextToken());
+                            while (st.hasMoreTokens()) {
+                                sum += Integer.parseInt(st.nextToken());
                             }
                             System.out.println(sum);
 
                             break;
                         case "sub(":
+                            System.out.println("- - - initLine - - -");
+                            int sub = 0;
+                            String dat = LineaCode.get(i + 1);
 
-                            int sub=0;
-                            String dat=LineaCode.get(i+1);
+                            int count = 0;
 
-                            int count=0;
+                            StringTokenizer sts = new StringTokenizer(dat, "-");
 
-                            StringTokenizer sts= new StringTokenizer(dat,"-");
-
-                            while (sts.hasMoreTokens()){
+                            while (sts.hasMoreTokens()) {
                                 count++;
-                                if(count==1){
-                                    sub+=Integer.parseInt(sts.nextToken());
-                                }else{
-                                    sub-=Integer.parseInt(sts.nextToken());
+                                if (count == 1) {
+                                    sub += Integer.parseInt(sts.nextToken());
+                                } else {
+                                    sub -= Integer.parseInt(sts.nextToken());
                                 }
                             }
                             System.out.println(sub);
                             break;
                         case "print(":
-                            System.out.println(LineaCode.get(i+1).toString().replace('"',' '));
+                            System.out.println("- - - initLine - - -");
+                            System.out.println(LineaCode.get(i + 1).toString().replace('"', ' '));
+                            break;
+                        case "mul(":
+                            System.out.println("- - - initLine - - -");
+                            String dt=LineaCode.get(i+1);
+                            int mul=1;
+                            StringTokenizer stm= new StringTokenizer(dt,"*");
+
+                            while(stm.hasMoreTokens()){
+                                mul*=Integer.parseInt(stm.nextToken());
+                            }
+                            System.out.println(mul);
+                            break;
+                        case ")":
+                            System.out.print("- - - endOfLine - - -\n");
                             break;
                         case "end;":
-                            System.out.println("Fin del programa! . ");
+                            System.out.println("\n>>End");
                             System.exit(0);
                             break;
                     }
